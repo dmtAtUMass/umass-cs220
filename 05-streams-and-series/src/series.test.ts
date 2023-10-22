37af222113ca5fb2da1593a4bd616bb4812b092e
dmtrung14 <trungdangminh14012004@gmail.com>
2023-10-21 22:10:58 -0400

    hw5

--- 05-streams-and-series/src/series.test.ts
+++ 05-streams-and-series/src/series.test.ts
@@ -1,5 +1,14 @@
 import { Stream, to } from "../include/stream.js";
-import { addSeries } from "./series.js";
+import {
+  addSeries,
+  derivSeries,
+  prodSeries,
+  coeff,
+  evalSeries,
+  recurSeries,
+  applySeries,
+  expSeries,
+} from "./series.js";
 
 function expectStreamToBe<T>(s: Stream<T>, a: T[]) {
   for (const element of a) {
@@ -10,6 +19,14 @@ function expectStreamToBe<T>(s: Stream<T>, a: T[]) {
   }
 
   expect(s.isEmpty()).toBe(true);
+}
+
+function expectStreamToBeginWith<T>(s: Stream<T>, a: T[]) {
+  for (const element of a) {
+    expect(s.isEmpty()).toBe(false);
+    expect(s.head()).toBe(element);
+    s = s.tail();
+  }
 }
 
 describe("addSeries", () => {
@@ -25,29 +42,87 @@ describe("addSeries", () => {
 });
 
 describe("prodSeries", () => {
-  // More tests for prodSeries go here
+  it("multiply simple streams together", () => {
+    // Open `include/stream.ts` to learn how to use `to`
+    // 1 -> 2 -> 3 -> 4 -> 5
+    const a = to(1, 3);
+    const b = to(1, 2);
+    const c = prodSeries(a, b);
+    expectStreamToBe(c, [1, 4, 7, 6]);
+  });
 });
 
 describe("derivSeries", () => {
-  // More tests for derivSeries go here
+  it("derivative of simple finite stream", () => {
+    // Open `include/stream.ts` to learn how to use `to`
+    // 1 -> 2 -> 3 -> 4 -> 5
+    const a = to(1, 5);
+    const c = derivSeries(a);
+
+    expectStreamToBe(c, [2, 6, 12, 20]);
+  });
 });
 
 describe("coeff", () => {
-  // More tests for coeff go here
+  it("get the first few terms of a simple stream", () => {
+    // Open `include/stream.ts` to learn how to use `to`
+    // 1 -> 2 -> 3 -> 4 -> 5
+    const a = to(1, 5);
+    for (let i = 0; i < 5; i++) {
+      const c = coeff(a, i);
+      expect(c).toEqual(Array.from({ length: i + 1 }, (_, i) => i + 1));
+    }
+  });
 });
 
 describe("evalSeries", () => {
-  // More tests for evalSeries go here
+  it("evaluate first few terms of simple stream", () => {
+    // Open `include/stream.ts` to learn how to use `to`
+    // 1 -> 2 -> 3 -> 4 -> 5
+    const a = to(1, 5);
+    const c = evalSeries(a, 3);
+    expect(c(1)).toEqual(10);
+    expect(c(2)).toEqual(49);
+  });
 });
 
 describe("applySeries", () => {
-  // More tests for applySeries go here
+  it("evaluate first few terms of simple stream", () => {
+    // Open `include/stream.ts` to learn how to use `to`
+    // 1 -> 2 -> 3 -> 4 -> 5
+    const a = 3;
+    const c = applySeries(x => x + 1, a);
+    expectStreamToBeginWith(c, [3, 4, 5, 6, 7]);
+  });
 });
 
 describe("expSeries", () => {
-  // More tests for expSeries go here
+  it("evaluate first few terms of taylor's series", () => {
+    expectStreamToBeginWith(expSeries(), [1, 1, 0.5, 0.16666666666666666, 0.041666666666666664]);
+  })
+
 });
 
 describe("recurSeries", () => {
   // More tests for recurSeries go here
+  it("should correctly apply recurSeries", () => {
+    const coef = [1, 3, 5, 7, 9];
+    const init = [2, 4, 6, 8, 10];
+    let res = recurSeries(coef, init);
+
+    const tmp: number[] = [];
+    for (let i = 0; i < coef.length * 3; i++) {
+      if (i < coef.length) {
+        tmp.push(init[i]);
+      } else {
+        let res = 0;
+        for (let j = 0; j < coef.length; j++) {
+          res += coef[j] * tmp[i - coef.length + j];
+        }
+        tmp.push(res);
+      }
+      expect(tmp[i]).toEqual(res.head());
+      res = res.tail();
+    }
+  });
 });
--- 05-streams-and-series/src/series.ts
+++ 05-streams-and-series/src/series.ts
@@ -1,43 +1,93 @@
-import { sempty, Stream } from "../include/stream.js";
+import { sempty, snode, Stream, from } from "../include/stream.js";
 
 export type Series = Stream<number>;
 
 export function addSeries(s: Series, t: Series): Series {
-  // TODO
-  return sempty();
+  if (s.isEmpty()) return t;
+  else if (t.isEmpty()) return s;
+  return snode(s.head() + t.head(), () => addSeries(s.tail(), t.tail()));
 }
 
 export function prodSeries(s: Series, t: Series): Series {
-  // TODO
-  return sempty();
+  if (s.isEmpty() || t.isEmpty()) return sempty();
+  return addSeries(
+    t.map(x => x * s.head()),
+    snode(0, () => prodSeries(s.tail(), t))
+  );
+}
+
+function prodOneOne(s: Series, t: Series): Series {
+  if (s.isEmpty() || t.isEmpty()) return sempty();
+  return snode(s.head() * t.head(), () => prodOneOne(s.tail(), t.tail()));
 }
 
 export function derivSeries(s: Series): Series {
-  // TODO
-  return sempty();
+  return prodOneOne(s, from(0)).tail();
+}
+
+function maskN(n: number): Stream<number> {
+  return n < 0 ? sempty() : snode(1, () => maskN(n - 1));
 }
 
 export function coeff(s: Series, n: number): number[] {
-  // TODO
-  return [];
+  let firstN = prodOneOne(s, maskN(n));
+  const arr = [];
+  while (!firstN.isEmpty()) {
+    arr.push(firstN.head());
+    firstN = firstN.tail();
+  }
+  return arr;
 }
 
 export function evalSeries(s: Series, n: number): (x: number) => number {
-  // TODO
-  return () => 0;
+  return x =>
+    coeff(s, n).reduce(
+      (acc: number[], i: number): number[] => {
+        return [acc[0] + i * Math.pow(x, acc[1]), acc[1] + 1];
+      },
+      [0, 0]
+    )[0];
 }
 
 export function applySeries(f: (c: number) => number, v: number): Series {
-  // TODO
-  return sempty();
+  return snode(v, () => applySeries(f, f(v)));
+}
+
+function invertFactorial(): (x: number) => number {
+  let count = 0;
+  return x => {
+    count += 1;
+    return x / count;
+  };
 }
 
 export function expSeries(): Series {
-  // TODO
-  return sempty();
+  return applySeries(invertFactorial(), 1);
+}
+
+function dotProduct(s: number[], t: number[]): number {
+  if (s.length === 0 || t.length === 0) return 0;
+  return s[0] * t[0] + dotProduct(s.slice(1), t.slice(1));
+}
+
+function recurCount(len_init: number, init: number[], coef: number[]): () => Series {
+  let k = 0;
+  const stream = [init[0]];
+  function something(): Series {
+    k += 1;
+    if (k < len_init) {
+      stream.push(init[k]);
+      return snode(init[k], () => something());
+    } else {
+      const res = snode(dotProduct(stream, coef), () => something());
+      stream.push(dotProduct(stream, coef));
+      stream.shift();
+      return res;
+    }
+  }
+  return something;
 }
 
 export function recurSeries(coef: number[], init: number[]): Series {
-  // TODO
-  return sempty();
+  return snode(init[0], () => recurCount(init.length, init, coef)());
 }
